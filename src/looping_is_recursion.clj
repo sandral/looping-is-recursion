22fd77f662d6faa956c5a13fe15c99360e51a6b7
Sandra Luhtaniemi <anni.luhtaniemi@helsinki.fi>
2014-12-08 10:16:28 +0200

    asdf

--- src/looping_is_recursion.clj
+++ src/looping_is_recursion.clj
@@ -1,13 +1,30 @@
 (ns looping-is-recursion)
 
 (defn power [base exp]
-  ":(")
+  (if (= exp 0)
+    1
+    (let [helper (fn [acc n k]
+                   (if (= 1 k)
+                     acc
+                     (recur (* acc n) n (dec k))))]
+      (helper base base exp))))
+
 
 (defn last-element [a-seq]
-  ":(")
+    (let [[x & xs] a-seq]
+      (if (empty? xs)
+        x
+        (last xs))))
 
 (defn seq= [seq1 seq2]
-  ":(")
+  (if (= (count seq1) (count seq2))
+    (let [[x & xs] seq1
+          [y & ys] seq2]
+      (if (and (empty? seq1) (empty? seq2))
+        true
+        (and (= x y) (seq= xs ys))))
+    false))
+
 
 (defn find-first-index [pred a-seq]
   ":(")
